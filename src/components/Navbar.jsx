bc55d7eca19559a3b2398e4f8182e252859e26a4
Alberto-Ibarra <alberto.e.ibarra@gmail.com>
2023-05-11 08:10:30 -0700

    updated resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import { BsFillMoonStarsFill } from 'react-icons/bs';
 const Navbar = () => {
     return (
             <nav className='py-10 mb-12 flex justify-between'>
-                <h1 className='text-2xl font-consolas'>Alberto Ibarra</h1>
+                <h1 className='text-2xl font-consolas'>AI</h1>
                 <ul className='flex items-center'>
                     {/* <li className='cursor-pointer text-2xl'><BsFillMoonStarsFill/></li> */}
-                    <li><a href='https://drive.google.com/file/d/16JvMzjOxx0F6XjysT7gwKNP7qNUFBrKL/view?usp=sharing' target='_blank' download className='bg-gradient-to-r from-teal-300 to-emerald-300 text-white px-4 py-2 rounded-md ml-8'>Resume</a></li>
+                    <li><a href='https://drive.google.com/file/d/110gDSiTMFyBuAJJoxAD3xMvfp5YswOAh/view?usp=sharing' target='_blank' download className='bg-gradient-to-r from-teal-300 to-emerald-300 text-white px-4 py-2 rounded-md ml-8'>Resume</a></li>
                 </ul>
             </nav>
     )
